33dfe00cabd4269183ff60e3a4b68333ffc71f1b
lamya2001 <144588377+lamya2001@users.noreply.github.com>
2024-10-19 23:39:56 +0300

    Add files via upload

---
+++ ExerciseTimeCalculator.java
@@ -0,0 +1,21 @@
+package com.mycompany.fitnesssystem;
+
+public class ExerciseTimeCalculator {
+    public static int calculateTime(int timeInPlan, String fitnessLevel) {
+        int additionalMinutes;
+        switch (fitnessLevel) {
+            case "Beginner":
+                additionalMinutes = 30;
+                break;
+            case "Intermediate":
+                additionalMinutes = 20;
+                break;
+            case "Advanced":
+                additionalMinutes = 10;
+                break;
+            default:
+                additionalMinutes = 0;
+        }
+        return timeInPlan + additionalMinutes;
+    }
+}
---
+++ FitnessSystem.java
@@ -0,0 +1,11 @@
+package com.mycompany.fitnesssystem;
+
+public class FitnessSystem {
+
+    public static void main(String[] args) {
+        HIITFitnessPlan f= new HIITFitnessPlan(1);
+        
+        f.displayPlanInfo();
+       
+    }
+}
