4dfd454473c4a505148cf31ac198540ddc8911ec
Lama <131875343+Lamaalbakri@users.noreply.github.com>
2024-10-20 15:18:59 +0300

    Update FitnessPlan.java

--- FitnessPlan.java
+++ FitnessPlan.java
@@ -14,15 +14,42 @@ public class FitnessPlan
         this.healthGoal = healthGoal;
     }
     
+    // Getters
+    public String getName() {
+        return name;
+    }
+    public int getMinDurationPerWeek() {
+        return minDurationPerWeek;
+    }
+    public String getMinFitnessLevel() {
+        return minFitnessLevel;
+    }
+    public String getHealthGoal() {
+        return healthGoal;
+    }
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setMinDurationPerWeek(int minDurationPerWeek) {
+        this.minDurationPerWeek = minDurationPerWeek;
+    }
+    public void setMinFitnessLevel(String minFitnessLevel) {
+        this.minFitnessLevel = minFitnessLevel;
+    }
+    public void setHealthGoal(String healthGoal) {
+        this.healthGoal = healthGoal;
+    }
+    
     public boolean isSuitable(String fitnessGoal, String fitnessLevel) {
         return this.healthGoal.equals(fitnessGoal) && this.minFitnessLevel.equals(fitnessLevel);
     }
     
     public void displayPlanDetails() {
-        System.out.println("\n\tFitness Plan: " + name);
-        System.out.println("\tMinimum Duration per Week: " + minDurationPerWeek + " minutes");
-        System.out.println("\tMinimum Required Fitness Level: " + minFitnessLevel);
-        System.out.println("\tHealth Goal: " + healthGoal);
+        System.out.println("\n\tFitness Plan: " + getName());
+        System.out.println("\tMinimum Duration per Week: " + getMinDurationPerWeek() + " minutes");
+        System.out.println("\tMinimum Required Fitness Level: " + getMinFitnessLevel());
+        System.out.println("\tHealth Goal: " + getHealthGoal());
         System.out.println();
     }
 }
