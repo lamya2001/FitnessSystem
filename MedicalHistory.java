4bfb5789017244f8b138c62dc50673b7e5077f1b
Lama <131875343+Lamaalbakri@users.noreply.github.com>
2024-10-20 14:58:47 +0300

    Update MedicalHistory.java

--- MedicalHistory.java
+++ MedicalHistory.java
@@ -3,7 +3,8 @@ package com.mycompany.fitnesssystem;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class MedicalHistory {
+public class MedicalHistory
+{
     private static ArrayList<String> healthNotes = new ArrayList<>();
     
     public static void collectMedicalHistory() {
@@ -15,20 +16,19 @@ public class MedicalHistory {
         int illnesses;
         int surgeries;
 
-//        System.out.println("----------Medical History------------");
-
         while (true) {
-            System.out.println("----------Age------------");
-            System.out.println("Enter your age: ");
+            System.out.println("\n----------Age------------");
+            System.out.println(">> Enter your age: ");
             String input = scanner.nextLine();
             age = Validator.getValidInteger(input);
             if (age >= 6 && age <= 18) {
-                 healthNotes.add("Do not lift heavy weights during exercise, and do not exceed one hour per day.");
+                 healthNotes.add("\tDo not lift heavy weights during exercise, and do not exceed one hour per day.");
                 break;
             } else if (age > 18 && age <= 60) {
+                healthNotes.add("\tMaintain a balanced workout routine, focusing on both strength and cardio exercises, and ensure proper workouts to prevent injuries.");
                 break;
             } else if (age > 60) {
-                healthNotes.add("Start exercising gradually, and take breaks when feeling pain.");
+                healthNotes.add("\tStart exercising gradually, and take breaks when feeling pain.");
                 break;
             } else {
                 System.out.println("Invalid age. Please enter a valid age \"6 and more\".");
@@ -36,15 +36,15 @@ public class MedicalHistory {
         }
         
         while (true) {
-            System.out.println("----------Illnesses------------");
-            System.out.println("Do you have any illnesses?:\n"
-                    + "1- yes\n"
-                    + "2- no");
+            System.out.println("\n----------Illnesses------------");
+            System.out.println(">> Do you have any illnesses?:\n"
+                    + "1- Yes\n"
+                    + "2- No");
             String inputIllness = scanner.nextLine();
             illnesses = Validator.getValidOption(inputIllness, 2);
             switch (illnesses) {
                 case 1:
-                    healthNotes.add("Discuss the proposed fitness plan with your doctor to ensure it suits your health condition.");
+                    healthNotes.add("\tDiscuss the proposed fitness plan with your doctor to ensure it suits your health condition.");
                     break;
                 case 2:
                     break;
@@ -56,17 +56,17 @@ public class MedicalHistory {
         }
         
         while (true) {
-            System.out.println("----------Surgeries------------");
-            System.out.println("Did you have any surgeries?:\n"
-                        + "1- yes\n"
-                        + "2- no");
+            System.out.println("\n----------Surgeries------------");
+            System.out.println(">> Did you have any surgeries?:\n"
+                        + "1- Yes\n"
+                        + "2- No");
 
             String inputSurgeries = scanner.nextLine();
             surgeries = Validator.getValidOption(inputSurgeries, 2);
 
             switch (surgeries) {
                 case 1:
-                    healthNotes.add("Ask your doctor about the appropriate time to start exercising after surgery and whether it is suitable for your health condition.");
+                    healthNotes.add("\tAsk your doctor when you should start exercising after surgery and whether it is suitable for your health condition.");
                     break;
                 case 2:
                     break;
