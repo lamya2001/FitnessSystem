9745f4a9e75ca2112b32837dece1474d35aae9c4
Jana Malibari <76265174+J29-h@users.noreply.github.com>
2024-10-24 05:15:25 +0300

    Update Yoga.java

--- Yoga.java
+++ Yoga.java
@@ -2,7 +2,7 @@
 package com.mycompany.fitnesssystem;
 
 class Yoga extends FitnessPlan {
-    public Yoga() {
+    protected Yoga() {
         super("Yoga", 120, "Beginner", "Stress Relief");
     }
 }
