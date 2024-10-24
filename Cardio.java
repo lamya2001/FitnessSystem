8a94b0578612ccb8b69e214f8a47caab800c85ab
Jana Malibari <76265174+J29-h@users.noreply.github.com>
2024-10-24 05:12:26 +0300

    Update Cardio.java

--- Cardio.java
+++ Cardio.java
@@ -1,6 +1,6 @@
 package com.mycompany.fitnesssystem;
 
-class Cardio extends FitnessPlan {
+package class Cardio extends FitnessPlan {
     public Cardio() {
         super("Cardio", 150, "Beginner", "Weight Loss");
     }
