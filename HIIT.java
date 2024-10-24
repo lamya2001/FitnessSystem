34f2e869105ce3b76feee5d50eaf94b658edf1b1
Jana Malibari <76265174+J29-h@users.noreply.github.com>
2024-10-24 05:16:37 +0300

    Update HIIT.java

--- HIIT.java
+++ HIIT.java
@@ -1,7 +1,7 @@
 package com.mycompany.fitnesssystem;
 
 public class HIIT extends FitnessPlan {
-    package HIIT() {
+    protected HIIT() {
         super("HIIT", 90, "Advanced", "Improve Cardiovascular Health");
     }
 }
